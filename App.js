d1ef028b82581fe0535ea69598a25742ee2260a7
Caster <44622004+Casterrr@users.noreply.github.com>
2021-10-09 17:06:50 -0300

    Requisição 2 da atividade
    Adiconando scrollview e outros componentes básicos.

--- App.js
+++ App.js
@@ -1,13 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.navbar}>
+        <Feather name="arrow-left" size={30} color="black" />
+        <Text style={styles.navbarText}>Lista de pessoas</Text>
+      </View>
+
+      <View style={styles.svcontainer}>
+        <ScrollView contentContainerStyle={styles.scrollview}>
+          
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Lucas Matheus</Text>
+              <Text>Idade: 18</Text>
+              <Text>Sexo: Masculino</Text>
+            </View>
+          </View>
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Mayra Cristina</Text>
+              <Text>Idade: 19</Text>
+              <Text>Sexo: Feminino</Text>
+            </View>
+          </View> 
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Lucas Matheus</Text>
+              <Text>Idade: 18</Text>
+              <Text>Sexo: Masculino</Text>
+            </View>
+          </View>
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Mayra Cristina</Text>
+              <Text>Idade: 19</Text>
+              <Text>Sexo: Feminino</Text>
+            </View>
+          </View> 
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Lucas Matheus</Text>
+              <Text>Idade: 18</Text>
+              <Text>Sexo: Masculino</Text>
+            </View>
+          </View>
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Mayra Cristina</Text>
+              <Text>Idade: 19</Text>
+              <Text>Sexo: Feminino</Text>
+            </View>
+          </View> 
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Lucas Matheus</Text>
+              <Text>Idade: 18</Text>
+              <Text>Sexo: Masculino</Text>
+            </View>
+          </View>
+
+          <View style={styles.userContainer}>
+            <View style={styles.userImg}></View>
+            <View style={styles.userData}>
+              <Text>Nome: Mayra Cristina</Text>
+              <Text>Idade: 19</Text>
+              <Text>Sexo: Feminino</Text>
+            </View>
+          </View> 
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +99,49 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
+  navbar:{
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 10
+  },
+  navbarText: {
+    fontSize: 25,
+    marginLeft: 10
+  },
+  svcontainer: {
+    height: '50%',
+    width: '95%',
+    
+    marginTop: 10
+  },
+  scrollview: {
+    flexDirection: 'column',
+    backgroundColor: '#f6f6f6',
+    padding: 10,
+    alignItems: 'center'
+  },
+  userContainer: {
+    width: '95%',
+    flexDirection: 'row',
+    backgroundColor: '#ededed',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 10
+  },
+  userImg: {
+    height: 53,
+    width: 53,
+    backgroundColor: '#a8a8a8',
+    borderRadius: 50,
+
+  },
+  userData: {
+    width: '70%',
+    marginLeft: 10
+  }
 });
